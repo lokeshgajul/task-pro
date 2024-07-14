f7762c1cbbcd6150762fc81c247e18209a4617e0
Lokesh Gajul <110080915+lokeshgajul@users.noreply.github.com>
2024-07-14 23:01:30 +0530

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -8,12 +8,12 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDBLbesUDGF4T0Sx5TAt19NCiPEZIRH3hE",
-  authDomain: "task-pro-ae6db.firebaseapp.com",
-  projectId: "task-pro-ae6db",
-  storageBucket: "task-pro-ae6db.appspot.com",
-  messagingSenderId: "457710720225",
-  appId: "1:457710720225:web:120f28c8eb6da5f2dc13a0",
+  apiKey: "**************************",
+  authDomain: "**********************",
+  projectId: "************************",
+  storageBucket: "********************",
+  messagingSenderId: "*****************",
+  appId: "******************************",
 };
 
 // Initialize Firebase
